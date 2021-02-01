b854d53115873beffacfe20ed44f690610534b36
Luke Rucker <ruckerlh@gmail.com>
2021-02-01 20:44:46 +0100

    Move endpoints into routes folder

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
-const Router = require('@koa/router')
-const isAuth = require('./middleware/is-auth')
 const errorHandler = require('./middleware/error-handler')
+const routes = require('./routes')
 
 const isProduction = process.env.NODE_ENV === 'production'
 
@@ -23,18 +22,7 @@ app.on('error', function (err, ctx) {
     }
 })
 
-// Add /api prefix in environments other than production, where it will be added via a proxy
-const router = new Router({
-    prefix: process.env.NODE_ENV !== 'production' ? '/api' : '',
-})
-
-router.use('/gym', require('./gym'))
-router.use('/auth', require('./auth'))
-router.use('/users', isAuth(), require('./users'))
-router.use('/members', isAuth(), require('./members'))
-router.use('/sessions', isAuth(), require('./sessions'))
-
-app.use(router.routes())
+app.use(routes)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const Router = require('@koa/router')
+const isAuth = require('../middleware/is-auth')
+
+// Add /api prefix in environments other than production, where it will be added via a proxy
+const router = new Router({
+    prefix: process.env.NODE_ENV !== 'production' ? '/api' : '',
+})
+
+router.use('/gym', require('./gym'))
+router.use('/auth', require('./auth'))
+router.use('/users', isAuth(), require('./users'))
+router.use('/members', isAuth(), require('./members'))
+router.use('/sessions', isAuth(), require('./sessions'))
+
+module.exports = router.routes()
