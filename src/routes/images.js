6c50f493e5b31bca613e2af5e7e6e57c7648c3d3
Luke Rucker <ruckerlh@gmail.com>
2021-03-10 15:16:35 +0100

    Update images.js

--- src/routes/images.js
+++ src/routes/images.js
@@ -6,13 +6,14 @@ const images = new Router()
 images.get('/:path*', async function (ctx) {
   const parts = ctx.params.path.split('/')
   const fileName = parts[parts.length - 1]
+
+  const s3 = new S3()
+  const file = await s3.downloadFile(ctx.params.path)
+
   ctx.response.set({
     'Cache-Control': `max-age=${60 * 10}`, // 10 min
     'Content-Disposition': `attachment; filename=${fileName}`,
   })
-
-  const s3 = new S3()
-  const file = await s3.downloadFile(ctx.params.path)
   ctx.body = file.Body
   ctx.status = 200
 })
