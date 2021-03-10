12f5fa4c2524963996b77c2a342a465bead98a68
Luke Rucker <ruckerlh@gmail.com>
2021-03-10 15:21:35 +0100

    Add logic to handle images not existing in S3

--- src/routes/images.js
+++ src/routes/images.js
@@ -7,15 +7,21 @@ images.get('/:path*', async function (ctx) {
   const parts = ctx.params.path.split('/')
   const fileName = parts[parts.length - 1]
 
-  const s3 = new S3()
-  const file = await s3.downloadFile(ctx.params.path)
+  try {
+    const s3 = new S3()
+    const file = await s3.downloadFile(ctx.params.path)
 
-  ctx.response.set({
-    'Cache-Control': `max-age=${60 * 10}`, // 10 min
-    'Content-Disposition': `attachment; filename=${fileName}`,
-  })
-  ctx.body = file.Body
-  ctx.status = 200
+    ctx.response.set({
+      'Cache-Control': `max-age=${60 * 10}`, // 10 min
+      'Content-Disposition': `attachment; filename=${fileName}`,
+    })
+    ctx.body = file.Body
+    ctx.status = 200
+  } catch (error) {
+    if (error.$metadata.httpStatusCode === 404) {
+      ctx.throw(404, 'Image does not exist.')
+    }
+  }
 })
 
 module.exports = images.routes()
